016d06e16a6d567a4f4ac2bc28c07d54fef14f02
Michele Matula <135138244+michele-matula@users.noreply.github.com>
2026-05-06 10:21:48 +0200

    feat(web): wire QueryClientProvider + AppLayout routes (WIP, pages stub)
    WIP commit: typecheck temporarily broken because /search and /vehicles/:id
    routes import @/pages/SearchResults and @/pages/VehicleDetail which are
    implemented in subsequent demo-2 tasks (T14, T15). Fixed in T15 commit.

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from '@/components/ui/sonner';
 import { App } from './App';
 import './globals.css';
 
@@ -9,5 +10,6 @@ if (!root) throw new Error('#root not found');
 createRoot(root).render(
   <StrictMode>
     <App />
+    <Toaster richColors position="top-right" />
   </StrictMode>,
 );
